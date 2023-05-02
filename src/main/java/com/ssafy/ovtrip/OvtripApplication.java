621dd5d8b38f028db6b8c45f6256e29bb3dccb4a
김기정 <cjdfidrlwjd@naver.com>
2023-05-02 11:59:01 +0900

    :tada: [Init] 프로젝트 생성

---
+++ src/main/java/com/ssafy/ovtrip/OvtripApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ovtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvtripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/ovtrip/OvtripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ovtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvtripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
