b9835fb95932f5c974f116d60656419aee5e166b
김지은 <jxixeun@gmail.com>
2023-05-04 17:00:37 +0900

    :white_check_mark: [Test] 회원가입 테스트 수정, ID로 회원 조회 테스트 작성

--- src/test/java/com/ovtrip/user/UserServiceTest.java
+++ src/test/java/com/ovtrip/user/UserServiceTest.java
@@ -1,6 +1,6 @@
 package com.ovtrip.user;
 
-import com.ovtrip.user.model.UserDto;
+import com.ovtrip.user.model.dto.UserDto;
 import com.ovtrip.user.service.UserService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -20,15 +20,23 @@ class UserServiceTest {
 
     @Test
     void 회원가입테스트() throws Exception {
-        int USER_ID = 5;
-        UserDto userDto = new UserDto();
-        userDto.setUserEmail("ustest22@test.com");
-        userDto.setUserNickname("testnickname");
-        userDto.setUserPassword("1234");
-        userDto.setSocialType("LOCAL");
+        int USER_ID = 8;
+        UserDto userDto = UserDto.builder()
+                .userEmail("ustest88@test.com")
+                .userNickname("테스트8")
+                .userPassword("1234")
+                .build();
         userService.joinUser(userDto);
         Assertions.assertThat(userService.getUserById(USER_ID).getUserEmail()).isEqualTo(userDto.getUserEmail());
         Assertions.assertThat(userService.getUserById(USER_ID).getUserNickname()).isEqualTo(userDto.getUserNickname());
-        Assertions.assertThat(userService.getUserById(USER_ID).getSocialType()).isEqualTo(userDto.getSocialType());
+        Assertions.assertThat(userService.getUserById(USER_ID).getSocialType()).isEqualTo("LOCAL");
+    }
+
+    @Test
+    void ID로_회원조회_테스트() throws Exception {
+        int USER_ID = 7;
+        Assertions.assertThat(userService.getUserById(USER_ID).getUserEmail()).isEqualTo("ustest77@test.com");
+        Assertions.assertThat(userService.getUserById(USER_ID).getUserNickname()).isEqualTo("테스트7");
+        Assertions.assertThat(userService.getUserById(USER_ID).getSocialType()).isEqualTo("LOCAL");
     }
 }
