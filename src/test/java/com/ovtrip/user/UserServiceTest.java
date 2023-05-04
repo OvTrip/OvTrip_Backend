3ee5580e2c3afd0e1d772d1042ed491989e72a63
김지은 <jxixeun@gmail.com>
2023-05-04 09:47:53 +0900

    :white_check_mark: [Test] 회원가입 테스트 작성

--- src/test/java/com/ovtrip/user/UserServiceTest.java
+++ src/test/java/com/ovtrip/user/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.ovtrip.user;
 
+import com.ovtrip.user.model.UserDto;
 import com.ovtrip.user.service.UserService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -15,5 +16,19 @@ class UserServiceTest {
     void 중복회원테스트() throws Exception {
         Assertions.assertThat(userService.idCheck("test@test.com")).isEqualTo(1);
         Assertions.assertThat(userService.idCheck("tdsfdsdf@test.com")).isEqualTo(0);
+    }
+
+    @Test
+    void 회원가입테스트() throws Exception {
+        int USER_ID = 5;
+        UserDto userDto = new UserDto();
+        userDto.setUserEmail("ustest22@test.com");
+        userDto.setUserNickname("testnickname");
+        userDto.setUserPassword("1234");
+        userDto.setSocialType("LOCAL");
+        userService.joinUser(userDto);
+        Assertions.assertThat(userService.getUserById(USER_ID).getUserEmail()).isEqualTo(userDto.getUserEmail());
+        Assertions.assertThat(userService.getUserById(USER_ID).getUserNickname()).isEqualTo(userDto.getUserNickname());
+        Assertions.assertThat(userService.getUserById(USER_ID).getSocialType()).isEqualTo(userDto.getSocialType());
     }
 }
