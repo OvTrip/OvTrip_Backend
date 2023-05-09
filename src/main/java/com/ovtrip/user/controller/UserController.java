f5c43ebbf593d771f61ea6d3648daf5ec947bd54
김지은 <jxixeun@gmail.com>
2023-05-09 09:51:45 +0900

    :zap: [Feat] 컨트롤러에 emailLogin 기능 구현 및 회원가입 리턴타입 ResponseEntity로 변경

--- src/main/java/com/ovtrip/user/controller/UserController.java
+++ src/main/java/com/ovtrip/user/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.ovtrip.user.controller;
 
-import com.ovtrip.user.model.UserDto;
+import com.ovtrip.user.model.dto.LoginUserDto;
+import com.ovtrip.user.model.dto.UserDto;
+import com.ovtrip.user.model.dto.UserVO;
 import com.ovtrip.user.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,34 +19,26 @@ public class UserController {
 
     @ApiOperation(value = "회원가입", notes = "회원 정보를 받아 새로운 회원을 생성합니다.")
     @PostMapping ("/user/auth/signup")
-    public CreateUserResponse signup(@RequestBody CreateUserRequest request) throws Exception {
-        UserDto userDto = new UserDto();
-        userDto.setUserEmail(request.getEmail());
-        userDto.setUserNickname(request.getNickname());
-        userDto.setUserPassword(request.getPassword());
-        userDto.setSocialType("LOCAL");
+    public ResponseEntity<?> signup(@RequestBody UserDto userDto) throws Exception {
         userService.joinUser(userDto);
-        return new CreateUserResponse(userDto.getUserEmail());
+        return new ResponseEntity<>(userDto.getUserEmail(), HttpStatus.CREATED);
     }
 
     @ApiOperation(value = "중복 회원 체크", notes = "이미 가입되어 있는 이메일의 경우 1을, 가입되어 있지 않은 이메일의 경우 0을 리턴합니다.")
     @GetMapping("/user/auth/{email}")
-    public ResponseEntity<Integer> idCheck(@PathVariable String email) throws Exception {
+    public ResponseEntity<?> idCheck(@PathVariable String email) throws Exception {
         int flag = userService.idCheck(email);
         return new ResponseEntity<>(flag, HttpStatus.OK);
     }
 
-    @Data
-    static class CreateUserRequest {
-        private String email;
-        private String nickname;
-        private String password;
-    }
-
-    @Data
-    @AllArgsConstructor
-    static class CreateUserResponse {
-        private String email;
+    @ApiOperation(value = "로그인", notes = "아이디, 비밀번호를 입력해 로그인을 진행합니다.")
+    @PostMapping ("/user/auth/login")
+    ResponseEntity<?> emailLogin(@RequestBody LoginUserDto loginUserDto) throws Exception {
+        UserVO userVO = userService.emailLogin(loginUserDto);
+        if (userVO == null){
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>(userVO, HttpStatus.OK);
     }
 
 }
