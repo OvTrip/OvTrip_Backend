3aff666d150fa78cc7960645cdc16d71379ea938
김지은 <jxixeun@gmail.com>
2023-05-09 09:52:32 +0900

    :white_check_mark: [Test] 로그인 성공, 실패 테스트 작성

--- src/test/java/com/ovtrip/user/UserServiceTest.java
+++ src/test/java/com/ovtrip/user/UserServiceTest.java
@@ -1,6 +1,8 @@
 package com.ovtrip.user;
 
+import com.ovtrip.user.model.dto.LoginUserDto;
 import com.ovtrip.user.model.dto.UserDto;
+import com.ovtrip.user.model.dto.UserVO;
 import com.ovtrip.user.service.UserService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -38,5 +40,30 @@ class UserServiceTest {
         Assertions.assertThat(userService.getUserById(USER_ID).getUserEmail()).isEqualTo("ustest77@test.com");
         Assertions.assertThat(userService.getUserById(USER_ID).getUserNickname()).isEqualTo("테스트7");
         Assertions.assertThat(userService.getUserById(USER_ID).getSocialType()).isEqualTo("LOCAL");
+    }
+
+    @Test
+    void 로그인테스트() throws Exception {
+        LoginUserDto loginUserDto = LoginUserDto.builder()
+                .userEmail("ustest88@test.com")
+                .userPassword("1234")
+                .socialType("LOCAL")
+                .build();
+        UserVO userVO = userService.emailLogin(loginUserDto);
+        Assertions.assertThat(userVO.getUserEmail()).isEqualTo(loginUserDto.getUserEmail());
+        Assertions.assertThat(userVO.getUserNickname()).isEqualTo("테스트8");
+        Assertions.assertThat(userVO.getUserId()).isEqualTo(8);
+        Assertions.assertThat(userVO.getSocialType()).isEqualTo(loginUserDto.getSocialType());
+    }
+
+    @Test
+    void 없는회원로그인() throws Exception {
+        LoginUserDto loginUserDto = LoginUserDto.builder()
+                .userEmail("ustest0322@test.com")
+                .userPassword("1234")
+                .socialType("LOCAL")
+                .build();
+        UserVO userVO = userService.emailLogin(loginUserDto);
+        Assertions.assertThat(userVO).isNull();
     }
 }
