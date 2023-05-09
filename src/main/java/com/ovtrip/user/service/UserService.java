dd8c6ab3306138eb479201f9cebf4c115f4dad0e
김지은 <jxixeun@gmail.com>
2023-05-09 09:50:49 +0900

    :zap: [Feat] UserService에 emailLogin 기능 구현

--- src/main/java/com/ovtrip/user/service/UserService.java
+++ src/main/java/com/ovtrip/user/service/UserService.java
@@ -1,11 +1,12 @@
 package com.ovtrip.user.service;
 
+import com.ovtrip.user.model.dto.LoginUserDto;
 import com.ovtrip.user.model.dto.UserDto;
 import com.ovtrip.user.model.dto.UserVO;
 
 public interface UserService {
     int idCheck(String userId) throws Exception;
     void joinUser(UserDto userDto) throws Exception;
-
     UserVO getUserById(int userId) throws Exception;
+    UserVO emailLogin(LoginUserDto loginUserDto) throws Exception;
 }
--- src/main/java/com/ovtrip/user/service/UserServiceImpl.java
+++ src/main/java/com/ovtrip/user/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ovtrip.user.service;
 
+import com.ovtrip.user.model.dto.LoginUserDto;
 import com.ovtrip.user.model.dto.UserDto;
 import com.ovtrip.user.model.dto.UserVO;
 import com.ovtrip.user.model.mapper.UserMapper;
@@ -26,6 +27,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserVO getUserById(int userId) throws Exception {
         return userMapper.getUserById(userId);
+    }
+
+    @Override
+    public UserVO emailLogin(LoginUserDto loginUserDto) throws Exception {
+        return userMapper.emailLogin(loginUserDto);
     }
 
 }
