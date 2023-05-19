7a4b291e551e96549261a6a49b1e6aad5dce9ebc
김지은 <jxixeun@gmail.com>
2023-05-19 17:15:40 +0900

    :zap: [Feat] Kakao 토큰 발급 요청을 위한 FeignClient 구현

---
+++ src/main/java/com/ovtrip/web/kakaotoken/client/KakaoTokenClient.java
@@ -0,0 +1,15 @@
+package com.ovtrip.web.kakaotoken.client;
+
+import com.ovtrip.web.kakaotoken.dto.KakaoTokenDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(url = "https://kauth.kakao.com", name = "kakaoTokenClient")
+public interface KakaoTokenClient {
+
+    @PostMapping(value = "/oauth/token", consumes = "application/json")
+    KakaoTokenDto.Response requestKakaoToken(@RequestHeader("Content-type") String contentType,
+                                             @SpringQueryMap KakaoTokenDto.Request request);
+}
