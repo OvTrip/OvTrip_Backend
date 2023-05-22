b7e568d8625807d7da97c78d56f55f2a406884be
김지은 <jxixeun@gmail.com>
2023-05-22 14:39:50 +0900

    :zap: [Feat] 존재하는 소셜 타입인지 반환하는 기능 구현

--- src/main/java/com/ovtrip/user/constant/SocialType.java
+++ src/main/java/com/ovtrip/user/constant/SocialType.java
@@ -1,5 +1,20 @@
 package com.ovtrip.user.constant;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum SocialType {
     EMAIL, KAKAO;
+
+    public static SocialType from(String type) {
+        return SocialType.valueOf(type.toUpperCase());
+    }
+
+    public static boolean isSocialType(String type) {
+        List<SocialType> socialTypes = Arrays.stream(SocialType.values())
+                .filter(socialType -> socialType.name().equals(type))
+                .collect(Collectors.toList());
+        return socialTypes.size() != 0;
+    }
 }
