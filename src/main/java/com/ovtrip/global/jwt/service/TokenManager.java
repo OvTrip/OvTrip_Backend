94c9e9b479cdd133f17075dbc2e8b5065c1a71ae
김지은 <jxixeun@gmail.com>
2023-05-22 11:00:00 +0900

    :zap: [Feat] 토큰의 생성, 발급, 검증을 진행하는 TokenManger 구현

---
+++ src/main/java/com/ovtrip/global/jwt/service/TokenManager.java
@@ -0,0 +1,100 @@
+package com.ovtrip.global.jwt.service;
+
+import com.ovtrip.global.error.ErrorCode;
+import com.ovtrip.global.error.exception.AuthenticationException;
+import com.ovtrip.global.jwt.constant.GrantType;
+import com.ovtrip.global.jwt.constant.TokenType;
+import com.ovtrip.global.jwt.dto.JwtTokenDto;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import com.ovtrip.user.constant.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class TokenManager {
+
+    //application.yml에 설정한 값을 가져온다.
+    private final String accessTokenExpirationTime;
+    private final String refreshTokenExpirationTime;
+    private final String tokenSecret;
+
+    public JwtTokenDto createJwtTokenDto(Long memberId, Role role) {
+        Date accessTokenExpireTime = createAccessTokenExpireTime();
+        Date refreshTokenExpireTime = createRefreshTokenExpireTime();
+        String accessToken = createAccessToken(memberId, role, accessTokenExpireTime);
+        String refreshToken = createRefreshToken(memberId, refreshTokenExpireTime);
+        return JwtTokenDto.builder()
+                .grantType(GrantType.BEARER.getType())
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .accessTokenExpireTime(accessTokenExpireTime)
+                .refreshTokenExpireTime(refreshTokenExpireTime)
+                .build();
+    }
+
+    public Date createAccessTokenExpireTime() {
+        return new Date(System.currentTimeMillis() + Long.parseLong(accessTokenExpirationTime));
+    }
+
+    public Date createRefreshTokenExpireTime() {
+        return new Date(System.currentTimeMillis() + Long.parseLong(refreshTokenExpirationTime));
+    }
+
+    public String createAccessToken(Long memberId, Role role, Date expirationTime) {
+        String accessToken = Jwts.builder()
+                .setSubject(TokenType.ACCESS.name())
+                .setIssuedAt(new Date())
+                .setExpiration(expirationTime)
+                .claim("memberId", memberId)
+                .claim("role", role)
+                .signWith(SignatureAlgorithm.HS512, tokenSecret.getBytes(StandardCharsets.UTF_8))
+                .setHeaderParam("typ", "JWT")
+                .compact();
+        return accessToken;
+    }
+
+    public String createRefreshToken(Long memberId, Date expirationTime) {
+        String refreshToken = Jwts.builder()
+                .setSubject(TokenType.REFRESH.name())
+                .setIssuedAt(new Date())
+                .setExpiration(expirationTime)
+                .claim("memberId", memberId)
+                .signWith(SignatureAlgorithm.HS512, tokenSecret.getBytes(StandardCharsets.UTF_8))
+                .setHeaderParam("typ", "JWT")
+                .compact();
+        return refreshToken;
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(tokenSecret.getBytes(StandardCharsets.UTF_8))
+                    .parseClaimsJws(token);
+        } catch (ExpiredJwtException e) {
+            log.info("token 만료", e);
+            throw new AuthenticationException(ErrorCode.TOKEN_EXPIRED);
+        } catch (Exception e) {
+            log.info("유효하지 않은 token", e);
+            throw new AuthenticationException(ErrorCode.NOT_VALID_TOKEN);
+        }
+    }
+
+    public Claims getTokenClaims(String token) {
+        Claims claims;
+        try {
+            claims = Jwts.parser().setSigningKey(tokenSecret.getBytes(StandardCharsets.UTF_8))
+                    .parseClaimsJws(token).getBody();
+        } catch (Exception e) {
+            log.info("유효하지 않은 token", e);
+            throw new AuthenticationException(ErrorCode.NOT_VALID_TOKEN);
+        }
+        return claims;
+    }
+}
