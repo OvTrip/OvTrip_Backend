aa4d635964a983c7196be22b6e88c84980b79001
김지은 <jxixeun@gmail.com>
2023-05-22 14:42:10 +0900

    :zap: [Feat] 소셜 타입에 맞는 서비스 구현체를 반환하는 Factory 구현

---
+++ src/main/java/com/ovtrip/external/oauth/service/SocialLoginApiServiceFactory.java
@@ -0,0 +1,23 @@
+package com.ovtrip.external.oauth.service;
+
+import com.ovtrip.user.constant.SocialType;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class SocialLoginApiServiceFactory {
+    private static Map<String, SocialLoginApiService> socialLoginApiServices;
+
+    public SocialLoginApiServiceFactory(Map<String, SocialLoginApiService> socialLoginApiServices) {
+        this.socialLoginApiServices = socialLoginApiServices;
+    }
+
+    public static SocialLoginApiService getSocialLoginApiService(SocialType socialType){
+        String socialLoginApiServiceBeanName = "";
+        if (SocialType.KAKAO.equals(socialType)){
+            socialLoginApiServiceBeanName = "kakaoLoginApiServiceImpl";
+        }
+        return socialLoginApiServices.get(socialLoginApiServiceBeanName);
+    }
+}
