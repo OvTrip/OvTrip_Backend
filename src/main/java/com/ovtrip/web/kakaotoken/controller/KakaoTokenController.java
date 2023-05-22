e1c7347c34c550dc917b95218fe6541a687dc3c3
김지은 <jxixeun@gmail.com>
2023-05-22 17:39:36 +0900

    :zap: [Feat] 인가 코드를 받고 Access Token을 백엔드에서 직접 요청해서 처리하도록 설정

--- src/main/java/com/ovtrip/web/kakaotoken/controller/KakaoTokenController.java
+++ src/main/java/com/ovtrip/web/kakaotoken/controller/KakaoTokenController.java
@@ -1,9 +1,13 @@
 package com.ovtrip.web.kakaotoken.controller;
 
+import com.ovtrip.login.dto.OAuthLoginDto;
+import com.ovtrip.login.service.OAuthLoginService;
+import com.ovtrip.user.constant.SocialType;
 import com.ovtrip.web.kakaotoken.client.KakaoTokenClient;
 import com.ovtrip.web.kakaotoken.dto.KakaoTokenDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class KakaoTokenController {
     private final KakaoTokenClient kakaoTokenClient;
+    private final OAuthLoginService oauthLoginService;
 
     @Value("${kakao.client.id}")
     private String clientId;
@@ -19,17 +24,19 @@ public class KakaoTokenController {
     @Value("${kakao.client.secret}")
     private String clientSecret;
 
-    @GetMapping("/oauth/kakao/callback")
-    public @ResponseBody String loginCallback(String code) {
+    @GetMapping("/oauth/kakao")
+    public @ResponseBody ResponseEntity<OAuthLoginDto.Response> kakaoLogin(String code) {
         String contentType = "application/x-www-form-urlencoded;charset=utf-8";
         KakaoTokenDto.Request kakaoTokenRequestDto = KakaoTokenDto.Request.builder()
                 .client_id(clientId)
                 .client_secret(clientSecret)
                 .grant_type("authorization_code")
                 .code(code)
-                .redirect_uri("http://localhost:8080/oauth/kakao/callback")
+                .redirect_uri("http://localhost:8081/oauth/kakao/callback")
                 .build();
         KakaoTokenDto.Response kakaoToken = kakaoTokenClient.requestKakaoToken(contentType, kakaoTokenRequestDto);
-        return "kakao token : " + kakaoToken;
+        String accessToken = kakaoToken.getAccess_token();
+        OAuthLoginDto.Response jwtTokenResponseDto = oauthLoginService.oauthLogin(accessToken, SocialType.KAKAO);
+        return ResponseEntity.ok(jwtTokenResponseDto);
     }
 }
