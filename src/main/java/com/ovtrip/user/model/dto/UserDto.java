054433c0f7444dec0eca899311fba3498dc3abcd
김지은 <jxixeun@gmail.com>
2023-05-22 10:56:49 +0900

    :zap: [Add] UserDto, UserVo에 필드 추가

--- src/main/java/com/ovtrip/user/model/dto/UserDto.java
+++ src/main/java/com/ovtrip/user/model/dto/UserDto.java
@@ -1,17 +1,25 @@
 package com.ovtrip.user.model.dto;
 
+import com.ovtrip.user.constant.Role;
+import com.ovtrip.user.constant.SocialType;
 import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class UserDto {
     private String userEmail;
     private String userPassword;
     private String userNickname;
-    private String socialType;
+    private SocialType socialType;
+    private String profileImg;
+    private Role role;
+    private String refreshToken;
+    private LocalDateTime tokenExpirationTime;
 
     @Builder
-    public UserDto(String userEmail, String userPassword, String userNickname, String socialType) {
+    public UserDto(String userEmail, String userPassword, String userNickname, SocialType socialType) {
         this.userEmail = userEmail;
         this.userPassword = userPassword;
         this.userNickname = userNickname;
--- src/main/java/com/ovtrip/user/model/dto/UserVO.java
+++ src/main/java/com/ovtrip/user/model/dto/UserVO.java
@@ -1,16 +1,26 @@
 package com.ovtrip.user.model.dto;
 
+import com.ovtrip.user.constant.Role;
+import com.ovtrip.user.constant.SocialType;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class UserVO {
     private int userId;
     private String userEmail;
     private String userNickname;
-    private String socialType;
+    private SocialType socialType;
+    private String profileImg;
+    private Role role;
+    private String refreshToken;
+    private LocalDateTime tokenExpirationTime;
+    private LocalDateTime regTime;
+    private LocalDateTime modTime;
 
     @Builder
-    public UserVO(int userId, String userEmail, String userNickname, String socialType) {
+    public UserVO(int userId, String userEmail, String userNickname, SocialType socialType) {
         this.userId = userId;
         this.userEmail = userEmail;
         this.userNickname = userNickname;
