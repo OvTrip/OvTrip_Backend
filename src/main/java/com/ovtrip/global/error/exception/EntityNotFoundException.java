d188396e55f0c967c4a52340870d2cfefcaee949
김지은 <jxixeun@gmail.com>
2023-05-24 11:11:09 +0900

    :zap: [Feat] EntityNotFoundException 생성

---
+++ src/main/java/com/ovtrip/global/error/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ovtrip.global.error.exception;
+
+import com.ovtrip.global.error.ErrorCode;
+
+public class EntityNotFoundException extends BusinessException{
+    public EntityNotFoundException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
